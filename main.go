d4305e191c11078c94b0c5b21ad21deb88a5bc0a
Akmyrza <onlyfordox@mail.ru>
2022-12-09 00:13:55 +0600

    completed

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
+)
 
 func main() {
-	fmt.Printf("%s has %d cars", "John", 2)
+
+	var offset int64 = 10
+	var limit int64 = 1024 * 1024 * 100
+	
+	inputFile, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("Can't open input file!")
+	}
+
+	outputFile, err := os.Create("output.txt")
+	if err!= nil {
+		log.Fatal("Can't open output file!")
+	}
+	
+	reader := io.NewSectionReader(inputFile, offset, limit)
+	writer := outputFile
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(reader)
+
+	io.Copy(writer, barReader)
+
+	bar.Finish()
+
+	defer outputFile.Close()
+	defer inputFile.Close()
 }
