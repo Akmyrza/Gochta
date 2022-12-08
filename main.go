055c6b37fa2602911e312eab8554028855e10dca
Akmyrza <106691920+Akmyrza@users.noreply.github.com>
2022-12-08 20:29:46 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"sync"
+)
+
+type Origin struct {
+	A int `json:"a"`
+	B int `json:"b"`
+	Average int
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	var origin []Origin
+	block := []int{}
+	result := []int{} 
+	var sum int = 0
+
+	content, err := ioutil.ReadFile("./input.json")
+	if err != nil {
+		fmt.Println("Cannot read file!")
+	}
+
+	err = json.Unmarshal([]byte(content), &origin)
+	if err != nil {
+		fmt.Println("Cannot parse file!")
+	}
+
+	for i, value := range origin {
+		block = append(block, value.A, value.B)
+		if i % 100000 == 0 {
+			wg.Add(1)
+			go func () {
+				defer wg.Done()
+				sum = 0
+				for _, v := range block {
+					sum += v
+				}
+				result = append(result, sum/len(block))
+				
+			} ()			
+			wg.Wait()
+			block = nil
+		}		
+	}
+	
+
+	sum = 0
+	for _, v := range result {
+		sum += v
+	}
+	
+	fmt.Println(sum/len(result))
+}
+
+
